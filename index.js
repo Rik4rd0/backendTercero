fab18f178df3505ec67deec9a43b2b2fd9fd292e
RICK <rickmartinezbanda@gmail.com>
2024-08-17 17:36:00 -0500

    actualizacion 6

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ dotenv.config();
 const app = express(); 
 const PORT = process.env.PORT || 5000;
 
-const express = require('express');
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // Permite solicitudes desde cualquier origen
