ed5c9c9d728eec7e443c9197b25dadfd94a62a2b
RICK <rickmartinezbanda@gmail.com>
2024-08-17 17:00:06 -0500

    actualizacion env

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ import express from 'express';
 import cors from 'cors';
 import db from './database/db.js';
 import terceroRoutes from './routes/routes.js';
+import dotenv from "dotenv";
+dotenv.config();
 
-const cors = require('cors');
-
-
-app.use(cors({
-    origin: 'https://tercero-1.onrender.com', // Reemplaza con el dominio de tu frontend
-    methods: 'GET,POST,PUT,DELETE',           // Métodos HTTP permitidos
-    credentials: true                         // Habilitar envío de cookies y credenciales
-}));
-
-const app = express();
+const app = express(); // Inicializa app antes de usarla
 const PORT = process.env.PORT || 5000;
+
+// Configura CORS antes de las rutas
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: 'GET,POST,PUT,DELETE',           
+    credentials: true                         
+}));
 
 app.use(express.json());
 app.use('/tercero', terceroRoutes);
 
 try {
-    db.authenticate();
+    await db.authenticate();
     console.log('Conexión a la base de datos exitosa');
 } catch (error) {
     console.log('Error en la conexión a la base de datos:', error);
@@ -27,5 +27,4 @@ try {
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    }
-);
+});
